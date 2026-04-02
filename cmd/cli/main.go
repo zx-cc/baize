f2297644f385cfa4e1f80a531901ad04679efa07
zenithax-cc <13326520186@163.com>
2026-04-02 11:00:15 +0800

    fix: memory/network/pci

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -3,17 +3,28 @@ package main
 import (
 	"encoding/json"
 
-	"github.com/zx-cc/baize/internal/collector/cpu"
+	"github.com/zx-cc/baize/internal/collector/memory"
+	"github.com/zx-cc/baize/internal/collector/network"
 )
 
-func main() {
-	c := cpu.New()
+type collector interface {
+	Collect() error
+}
 
-	if err := c.Collect(); err != nil {
+func main() {
+	n := network.New()
+	m := memory.New()
+
+	print(n)
+	print(m)
+}
+
+func print(n collector) {
+	if err := n.Collect(); err != nil {
 		panic(err)
 	}
 
-	res, err := json.MarshalIndent(c, "", "  ")
+	res, err := json.MarshalIndent(n, "", "  ")
 	if err != nil {
 		panic(err)
 	}
